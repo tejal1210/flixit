71dfd97c703ee32b7650bb11dece8cd60687b933
Tejal Shirsat <tejalshirsat94@gmail.com>
2024-07-09 04:29:42 +0530

    added controller to refresh access token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import {
     loginUser, 
     logoutUser, 
     registerUser, 
-    // refreshAccessToken, 
+    refreshAccessToken, 
     // changeCurrentPassword, 
     // getCurrentUser, 
     // updateUserAvatar, 
@@ -33,5 +33,6 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
