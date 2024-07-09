0feb69036ac1cf0bdb20ea7eae6c052b68e56316
Tejal Shirsat <tejalshirsat94@gmail.com>
2024-07-09 15:40:29 +0530

    added controller to fetch user profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,9 +8,9 @@ import {
     getCurrentUser,
     updateAccountDetails, 
     updateUserAvatar, 
-    // updateUserCoverImage, 
-    // getUserChannelProfile, 
-    // getWatchHistory,
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory,
 } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -39,10 +39,10 @@ router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-// router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
-// router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
-// router.route("/history").get(verifyJWT, getWatchHistory)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 export default router
